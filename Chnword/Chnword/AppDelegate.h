cfa9b9e3de9f3cf85db98a2f33118d54cb64a9de
kaikaiwei <137067401@qq.com>
2015-04-26 23:09:02 +0800

    init

---
+++ Chnword/Chnword/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Chnword
+//
+//  Created by khtc on 15/4/26.
+//  Copyright (c) 2015年 chnword. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Chnword/Chnword/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Chnword
+//
+//  Created by khtc on 15/4/26.
+//  Copyright (c) 2015年 chnword. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
