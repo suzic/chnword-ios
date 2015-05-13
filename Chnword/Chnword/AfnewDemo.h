cfada1c4f518fb8f18063fd08f8e40b3bf9bbc29
kaikaiwei <137067401@qq.com>
2015-05-13 08:41:46 +0800

    before net

---
+++ Chnword/Chnword/AfnewDemo.h
@@ -0,0 +1,13 @@
+//
+//  AfnewDemo.h
+//  Chnword
+//
+//  Created by khtc on 15/5/11.
+//  Copyright (c) 2015年 chnword. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AfnewDemo : NSObject
+
+@end
